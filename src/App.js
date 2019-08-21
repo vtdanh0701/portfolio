7025aa69f3b1eca7116dfaa2dd4a3046c9d2f21c
Danh Vuong <danhvuong@Danhs-Macbook.local>
2019-08-20 17:00:39 -0700

    about page styled

--- src/App.js
+++ src/App.js
@@ -2,31 +2,45 @@ import React from 'react';
 import './App.css';
 import './Nav.css';
 import './Animate.css'
+import logos from './static/logos';
 import AOS from 'aos';
 import Pdf from './static/Danh_Vuong_resume.pdf';
 import profile from './static/profile1.jpg';
-import reactLogo from './static/react-logo.png';
-import reactRouter from './static/react-router.svg';
-import nodeLogo from './static/nodejs-icon.svg';
-import expressLogo from './static/express-logo.png';
-import postgresLogo from './static/postgresql-inc.svg';
-import sequelizeLogo from './static/sequelize.svg';
-import mongoDbLogo from './static/mongodb-original.svg';
-import pythonLogo from './static/python.svg';
-import djangoLogo from './static/django.svg';
-import htmlLogo from './static/04H9yU7gSxVBEPrwuDEy4G1if.png';
-import cssLogo from './static/css3.svg';
-import gitLogo from './static/git-icon.svg';
-import gitHubLogo from './static/github-icon-1.svg';
-import jsLogo from './static/javascript-1.svg';
-import vsLogo from './static/visual-studio-code.svg';
-import psLogo from './static/photoshop-cc.svg';
-import premiereLogo from './static/premiere-cc.svg';
 import project1 from './static/project1.png';
 import project2 from './static/project2.png';
 import {Link} from 'react-scroll';
 AOS.init();
 function App() {
+  let skills=[];
+  let tech=[
+    'React',
+    'React Router',
+    'Node.js',
+    'Express',
+    'PostgreSQL',
+    'Sequelize',
+    'MongoDB',
+    'Python',
+    'Django',
+    'HTML5',
+    'CSS3',
+    'JavaScript',
+    'Git',
+    'Github',
+    'VS Code',
+    'Photoshop',
+    'Premiere'
+  ]
+  let i = 0;
+  for( let logo in logos){
+    skills.push(
+      <div key={i} className='skills-node'>
+        <img src={logos[logo]} alt={logo}/>
+        <p>{tech[i]}</p>
+      </div>
+    );
+    i++;
+  }
   return (
     <div className="App">
       <main className='main'>
@@ -61,38 +75,23 @@ function App() {
           </div>
         </div>
         <div className='about'>
-          <div className='info'  data-aos="fade-up" data-aos-duration="1200" data-aos-easing="ease-in-out" data-aos-once="true">
-            <h1>About Me</h1>
-            <p>I'm a Seattle-based full-stack Web Developer who has broad experience with various   development technologies, and I am currently specializing in JavaScript and React. </p>
-            <p>I enjoy learning new things and finding creative solutions for out of the ordinary   problems. Passionate and inspired by visual aesthetics, good design, and clean, precise   code. Driven to create interactive products shaped around human experience, expression,   and intuitive, efficient functionality.</p>
-            <p>Being a self-taught pianist and a conductor for my community church choir taught me   teamwork, communication and discipline. Working with various team refined my empathy,   strategy and problem-solving. Learning programming on my own, I learned patience,   perseverance and persistence. I translate those valuable skills to my work environment,   which allows me to offer unique and creative approaches to any project or challenge.</p>
-            <p>Curious to learn more about me ? Feel free to reach out!</p>
+          <div className='about-container' data-aos="fade-up" data-aos-duration="1200" data-aos-easing="ease-in-out" data-aos-anchor-placement="top-center">
+          <h1>About Me</h1>
+            <div className='info'>
+              
+              <p>I'm a Seattle-based full-stack Web Developer who has broad experience with various   development technologies, and I am currently specializing in JavaScript and React. </p>
+              <p>I enjoy learning new things and finding creative solutions for out of the ordinary   problems. Passionate and inspired by visual aesthetics, good design, and clean, precise   code. Driven to create interactive products shaped around human experience, expression,   and intuitive, efficient functionality.</p>
+              <p>Being a self-taught pianist and a conductor for my community church choir taught me   teamwork, communication and discipline. Working with various team refined my empathy,   strategy and problem-solving. Learning programming on my own, I learned patience,   perseverance and persistence. I translate those valuable skills to my work environment,   which allows me to offer unique and creative approaches to any project or challenge.</p>
+              <p>Curious to learn more about me ? Feel free to reach out!</p>
+            </div>
+            <div className='profile-container'>
+              <img src={profile}/>
+            </div>
+          
+            <div className='skills'>
+              {skills}
+            </div>
           </div>
-          <div className='profile-container'>
-            <img src={profile}/>
-          </div>
-
-          <div className='skills'>
-              <img src={reactLogo}/>
-              <img src={reactRouter}/>
-              <img src={nodeLogo}/>
-              <img src={expressLogo}/>
-              <img src={postgresLogo}/>
-              <img src={sequelizeLogo}/>
-              <img src={mongoDbLogo}/>
-              <img src={pythonLogo}/>
-              <img src={djangoLogo}/>
-              <img src={jsLogo}/>
-              <img src={htmlLogo}/>
-              <img src={cssLogo}/>
-              <img src={gitLogo}/>
-              <img src={gitHubLogo}/>
-              <img src={vsLogo}/>
-              <img src={psLogo}/>
-              <img src={premiereLogo}/>
-
-          </div>
-        
         </div>
         <div className='project'>
           <h1>My Project</h1>
---
+++ src/static/logos.jsx
@@ -0,0 +1,48 @@
+import reactLogo from './react-logo.png';
+import reactRouter from './react-router.svg';
+import nodeLogo from './nodejs-icon.svg';
+import expressLogo from './express-logo.png';
+import postgresLogo from './postgresql-inc.svg';
+import sequelizeLogo from './sequelize.svg';
+import mongoDbLogo from './mongodb-original.svg';
+import pythonLogo from './python.svg';
+import djangoLogo from './django.svg';
+import htmlLogo from './04H9yU7gSxVBEPrwuDEy4G1if.png';
+import cssLogo from './css3.svg';
+import gitLogo from './git-icon.svg';
+import gitHubLogo from './github-icon-1.svg';
+import jsLogo from './javascript-1.svg';
+import vsLogo from './visual-studio-code.svg';
+import psLogo from './photoshop-cc.svg';
+import premiereLogo from './premiere-cc.svg';
+
+export default [
+    reactLogo,
+    reactRouter,
+    nodeLogo,
+    expressLogo,
+    postgresLogo,
+    sequelizeLogo,
+    mongoDbLogo,
+    pythonLogo,
+    djangoLogo,
+    htmlLogo,
+    cssLogo,
+    jsLogo,
+    gitLogo,
+    gitHubLogo,
+    vsLogo,
+    psLogo,
+    premiereLogo
+]
+
+// let i = 0;
+//   for( let logo in logos){
+//     skills.push(
+//       <div key={i}>
+//         <img src={logo} alt={logo}/>
+//         <p>{tech[i]}</p>
+//       </div>
+//     );
+//     i++;
+//   }
